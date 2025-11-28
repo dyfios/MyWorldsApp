c9bb2036f5ae88c90e2156103b3c196ebb83a137
Dylan Baker <dbaker5525@gmail.com>
2025-11-28 15:26:55 -0500

    Please

--- src/modules/InputRouter.ts
+++ src/modules/InputRouter.ts
@@ -286,7 +286,7 @@ export class InputRouter {
         // console and settings
     }
     else {
-      (globalThis as any).messageEditToolbar('CONTEXT_MENU_TOGGLE_MENU');
+      (globalThis as any).toggleVRToolbar();
     }
   }
 
